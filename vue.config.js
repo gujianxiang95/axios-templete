d04835c7f5e3d1ef937c70ad7c1bfbfe2abc75e0
顾建祥 <gu_jian_xiang@126.com>
2020-11-13 10:01:25 +0800

    解决跨域问题

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+
+const path = require("path");
+
+module.exports = {
+    publicPath: '/august_wind',
+    outputDir: 'dist',
+    assetsDir: 'static',
+    productionSourceMap: false,
+    lintOnSave: false,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+                // library: path.resolve(__dirname, "common-frontend/src")
+            }
+        }
+    },
+    devServer: {
+        open: true,
+        overlay: {
+            warnings: false,
+            errors: true
+        },
+        proxy: {
+            '/dev-api': {
+              target: `http://106.14.42.109:3000`,
+              changeOrigin: true,
+              pathRewrite: {
+                '^/dev-api': 'http://106.14.42.109:3000'
+              }
+            }
+        }
+    }
+};
