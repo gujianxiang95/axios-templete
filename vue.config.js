20eab2d8a939ae8863ddf6ce7a77cf547e079d77
顾建祥 <gu_jian_xiang@126.com>
2020-11-23 16:37:20 +0800

    更改公共文件目录

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-    publicPath: '/jenkinTest/dist',
+    publicPath: '/jenkinTest',
     outputDir: 'dist',
     assetsDir: 'static',
     productionSourceMap: false,
